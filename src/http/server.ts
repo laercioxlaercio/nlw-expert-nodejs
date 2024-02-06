88d87f7fa88e769f6e9dced04ba7197704d9c3dc
Laercio Nascimento <laercio@protonmail.ch>
2024-02-06 19:52:49 -0300

    add poll creation route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,31 @@
 import fastiy from 'fastify';
+import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
 const config = {
   port: process.env.PORT || 3333,
 };
 
 const app = fastiy();
 
-app.get('/', () => 'ok');
+// connect db
+const prisma = new PrismaClient();
+
+// create new poll
+app.post('/polls', async (request, reply) => {
+  const createPollBody = z.object({
+    title: z.string(),
+  });
+
+  const { title } = createPollBody.parse(request.body);
+
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    },
+  });
+
+  return reply.status(201).send({ pollId: poll.id });
+});
 
 app
   .listen({ port: Number(config.port) })
